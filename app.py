a92dd0df988fce0fd3805a5cd6446738d844f3f3
Luislopeze <luislopeze24@gmail.com>
2026-08-27 15:09:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from models import db
 
 app = Flask(__name__)
 
-# Pega aquí la URL que Render te da al crear la base PostgreSQL
+# URL de conexión que Render te dio para tu base PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clinica_db_ov6b_user:l2yxIhbi371H74I5HVh8B69581fJ1iOI@dpg-da8b3q0ae00c73cd5sog-a/clinica_db_ov6b"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+# Crear tablas automáticamente la primera vez que entres
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route("/")
 def index():
