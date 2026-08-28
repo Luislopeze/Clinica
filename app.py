870dd8ae9a692a1a3b4cd56820ed70d98ce9f8d0
Luislopeze <luislopeze24@gmail.com>
2026-08-27 17:59:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import date
 
 app = Flask(__name__)
-app.secret_key = "clinica_secret"  # Necesario para mensajes flash
+app.secret_key = "clinica_secret"
 
 # Datos simulados
 pacientes = []
