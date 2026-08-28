85e6b4f478a7efc6fea87eb87bf00fa23c539852
Luislopeze <luislopeze24@gmail.com>
2026-08-27 17:25:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Clinica funcionando ✅"
+    hoy = date.today().strftime("%Y-%m-%d")
+    odontologos = [
+        {"nombre": "Luis", "citas": 0},
+        {"nombre": "Angie", "citas": 0}
+    ]
+    citas = []
+    return render_template("index.html", hoy=hoy, odontologos=odontologos, citas=citas)
